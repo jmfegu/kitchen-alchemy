2541e4137880ca31c17468c599b5225419e8f830
jmfegu <jmfernandez@emergya.com>
2023-03-01 23:03:04 +0100

    #Configuracion-base: Theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { KitchenApp } from './KitchenApp';
+import { kitchenTheme } from './theme/kitchenTheme';
 
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <KitchenApp />
+      <ThemeProvider theme={ kitchenTheme }>
+        <CssBaseline/>
+        <KitchenApp />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
